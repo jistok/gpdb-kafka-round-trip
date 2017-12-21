9b07b0cf6fbff0b26ae26cc31750a60d5e142012
Michael Goddard <mgoddard@gopivotal.com>
2017-12-21 08:59:23 -0500

    Accepting topic name as command line argument

--- kafka_delete_topic.sh
+++ kafka_delete_topic.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-topic="chicago_crimes"
+if [ "$#" -ne 1 ]
+then
+  echo "Usage: $0 topic_name"
+  exit 1
+fi
+
+topic=$1
 
 . ./kafka_env.sh
 $kafka_dir/bin/kafka-topics.sh --delete --topic $topic --zookeeper $zk_host:2181
